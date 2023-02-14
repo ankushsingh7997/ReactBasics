397f93e33d4331bda1bfd9e1c45149adb2aa2d08
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-02-15 05:06:30 +0530

    onchange Event

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@
 import './App.css'
 import React,{useState} from 'react'
 
-// function base increase marks 
 
 
+
+
+function EventHandler()
+{
+  const[data,setData]=useState('')
+
+  function handleEvent(e)
+  {
+     setData(e.target.value)
+    
+  }
+
+ return <div>
+  <input type={"text"} value={data} onChange={handleEvent}></input>
+ </div>
+}
 
 
 
@@ -15,34 +30,15 @@ import React,{useState} from 'react'
 let app='App'
 function App()
 {
-  const[city,SetCity]=useState("")
-  function City()
-  {return <div>
-   my city is : {city}
-  </div>}
-
-  function Setcit(mycity)
-  {
-    SetCity(mycity)
-  }
+  
   return <div className={app}>
     <div  >
-    
-    
-    <City/>
-    <Changecity Setcit={Setcit}/>
+      <EventHandler/>
     </div>
   </div>
 }
 
-function Changecity(props)
-{
-  let mycity="srinagar"
-  return <div>
-  <button onClick={()=>{props.Setcit(mycity)}}> click here</button>
- 
-  </div>
-}
+
 
 export default App
 
