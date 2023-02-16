27641b9d89a8b92c57dbd5ffb6dfed646a34dc67
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-02-17 03:13:02 +0530

    registration

--- src/App.js
+++ src/App.js
@@ -4,31 +4,77 @@
 import './App.css'
 import React,{useState} from 'react'
 
-
-function Countclick()
+function Regestration()
 {
-  
-   let [count,SetCount]=useState(0)
-   const[random,SetRandom]=useState(0)
-   function HandleFun()
-   {
-    SetRandom(Math.floor(Math.random()*10))
-    SetCount(count+1)
-    if(random==7) {alert(`number metches in ${count+1} clicks`)
-SetCount(0)
-   }
-   }
-  return <div>
-    <div>
-    
-    </div>
-    {count}
-    <div>
-    <button onClick={HandleFun}>clickMe</button>
-    </div>
+const[firstName,SetFirstName]=useState("")
+const[lastName,SetLastName]=useState("")
+const[gender,SetGender]=useState("")
+const[country,Setcountry]=useState("India")
+const[email,Setemail]=useState(false)
 
-  </div>
+function handleSubmit(e)
+{
+e.preventDefault()
+  if(firstName&&lastName&&gender&& country&&email) {console.log("regestered successfully")
+   console.log(`firstName : ${firstName}`)
+   console.log(`lastName : ${lastName}`)
+   console.log(`gender : ${gender}`)
+   console.log(`country : ${country}`)
+   console.log(`email : ${email}`)
 }
+   else{alert("please fill all the fields")}
+}
+
+
+return <div>
+<form>
+  <div>
+  <section>
+<label>firstName</label>
+<input type={'text'} value={firstName} onChange={(e)=>{SetFirstName(e.target.value.trim())}}></input>
+
+  </section>
+  <section>
+<label>lastName</label>
+<input type={'text'} value={lastName} onChange={(e)=>{SetLastName(e.target.value.trim())}}></input>
+
+  </section>
+  <section style={{display:"flex",justifyContent:'space-evenly'}}>
+<label>gender</label>
+<label > male</label>
+<input type={'radio'} name='gender' value={'male'} onChange={(e)=>{SetGender(e.target.value)}}></input>
+<label> female</label>
+<input type={'radio'} name='gender' value={'female'} onChange={(e)=>{SetGender(e.target.value)}}></input>
+
+  </section>
+  <section>
+<label>Country</label>
+<select onChange={(e)=>{Setcountry(e.target.value)}}>
+<option value={'India'}>India</option>
+<option value={'China'}>China</option>
+<option value={'Pakistan'}>Pakistan</option>
+<option value={'Russia'}>Russia</option>
+<option value={'Nepal'}>Nepal</option>
+
+</select>
+
+  </section>
+  <section>
+<label>Do you want to recieve email </label>
+<input type={'checkbox'} value={email} onChange={(e)=>{Setemail(!email)}} ></input>
+
+
+  </section>
+  <section><button onClick={handleSubmit}>Submit</button></section>
+  </div>
+  
+</form>
+
+
+
+</div>
+}
+
 
 
 let app='App'
@@ -37,7 +83,7 @@ function App()
   
   return <div className={app}>
     <div  >
-    <Countclick/>
+    <Regestration/>
     </div>
   </div>
 }
