5780c5944d6c73361fd9a914e5adc6005e0340cd
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-07-13 21:57:31 +0530

    imageupload

--- src/App.js
+++ src/App.js
@@ -1,90 +1,51 @@
-// import logo from './logo.svg';
-// import './App.css';
-
+import React,{useState,useRef} from 'react'
 import './App.css'
-import React,{useState} from 'react'
-
-function Regestration()
-{
-const[firstName,SetFirstName]=useState("")
-const[lastName,SetLastName]=useState("")
-const[gender,SetGender]=useState("")
-const[country,Setcountry]=useState("India")
-const[email,Setemail]=useState(false)
-
-function handleSubmit(e)
-{
-e.preventDefault()
-  if(firstName&&lastName&&gender&& country&&email) {console.log("regestered successfully")
-   console.log(`firstName : ${firstName}`)
-   console.log(`lastName : ${lastName}`)
-   console.log(`gender : ${gender}`)
-   console.log(`country : ${country}`)
-   console.log(`email : ${email}`)
-}
-   else{alert("please fill all the fields")}
-}
 
 
-return <div>
-<form>
-  <div>
-  <section>
-<label>firstName</label>
-<input type={'text'} value={firstName} onChange={(e)=>{SetFirstName(e.target.value.trim())}}></input>
-
-  </section>
-  <section>
-<label>lastName</label>
-<input type={'text'} value={lastName} onChange={(e)=>{SetLastName(e.target.value.trim())}}></input>
-
-  </section>
-  <section style={{display:"flex",justifyContent:'space-evenly'}}>
-<label>gender</label>
-<label > male</label>
-<input type={'radio'} name='gender' value={'male'} onChange={(e)=>{SetGender(e.target.value)}}></input>
-<label> female</label>
-<input type={'radio'} name='gender' value={'female'} onChange={(e)=>{SetGender(e.target.value)}}></input>
-
-  </section>
-  <section>
-<label>Country</label>
-<select onChange={(e)=>{Setcountry(e.target.value)}}>
-<option value={'India'}>India</option>
-<option value={'China'}>China</option>
-<option value={'Pakistan'}>Pakistan</option>
-<option value={'Russia'}>Russia</option>
-<option value={'Nepal'}>Nepal</option>
-
-</select>
-
-  </section>
-  <section>
-<label>Do you want to recieve email </label>
-<input type={'checkbox'} value={email} onChange={(e)=>{Setemail(!email)}} ></input>
-
-
-  </section>
-  <section><button onClick={handleSubmit}>Submit</button></section>
-  </div>
-  
-</form>
-
-
-
-</div>
-}
-
-
-
-let app='App'
 function App()
 {
+  const input=useRef(null)
   
-  return <div className={app}>
-    <div  >
-    <Regestration/>
+  const[image,setImage]=useState('')
+  const[file,setFile]=useState(null)
+  const handleImage=(e)=>{
+    setImage(e.target.files[0])
+    
+    
+  }
+  const handleClick=()=>{
+    input.current.click();
+    
+    
+
+  }
+  const handleSubmit=()=>{
+    if(image){
+    let reader=new FileReader()
+    reader.readAsDataURL(image)
+
+    reader.onloadend=()=>{
+      setFile(reader.result)
+    }
+  }
+  console.log(file)
+    
+   
+    
+    
+
+  }
+
+  
+  return <div className='maincont'>
+    <div onClick={handleClick}className='main'>
+      {image?<img src={URL.createObjectURL(image)} alt="image must be curropted"/>:
+      
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_rwFHWUOYHzwhzoOYei7bEOOc2A4ny50zmQ&usqp=CAU' alt="image must be curropted"/>}
+   <input type='file' ref={input}name="file" onChange={handleImage} style={{display:'none'}}/>
+   
     </div>
+    <button onClick={handleSubmit}>submit</button>
   </div>
 }
 
--- src/index.js
+++ src/index.js
@@ -2,27 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// const CurrentTime=()=>
-// {
-//   return <div> 
-//     <h1>{+new Date()}</h1>
-//   </div>
-// }
-// setInterval(()=>{root.render(
-//   <React.StrictMode>
-//     {/* <App /> */}
-//     {/* <h1>hello this is me</h1> */}
-//     <CurrentTime/>
-    
-//   </React.StrictMode>
-// )},1000)
+
 root.render(
   <React.StrictMode>
     <App />
-    {/* <h1>hello this is me</h1> */}
+
     
     
   </React.StrictMode>
@@ -30,7 +17,4 @@ root.render(
 
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
