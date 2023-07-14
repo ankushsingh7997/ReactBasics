827b43377f56802469e34c6b64eee657c9b35d92
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-07-15 02:07:13 +0530

    timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,38 @@
-import React,{useState,useRef, useEffect} from 'react'
+import React,{useState, useEffect} from 'react'
 import './App.css'
+
 
 
 function App()
 {
-  const[files,setFiles]=useState(null)
-  const[image,setImage]=useState('')
-  const input=useRef('')
-  const handleDragOver=(e)=>{
-    e.preventDefault();
-    
-    
-  }
-  const handleDrop=(e)=>{
-    e.preventDefault();
-   setFiles(e.dataTransfer.files[0]) 
-  }
-  
-  const clickSelectFile=()=>{
-    input.current.click();
-    
-
-  }
-  const handlefile=(e)=>{
-   
-    setFiles(e.target.files[0])
-  
-}
-const handleSubmit=()=>{
+  const[timer,setTimer]=useState(0)
+  const handleTimer=()=>{
  
-    if(files){
-      let reader=new FileReader()
-      reader.readAsDataURL(files)
+    
+  }
+  useEffect(()=>{
+    let interval=setInterval(()=>{
+      setTimer(timer+1)
+    },1000)
   
-      reader.onloadend=()=>{
-        setImage(reader.result)
-      }
-      setFiles(null)
-    alert('file uploaded successfully')
-    }
-    else alert('no file to upload')
-    
-    
 
-}
-useEffect(()=>{
-  console.log(image)
-})
+   return()=> clearInterval(interval)
+
+
+
+  })
+  
   
   return( 
   <>
-    <div className='main' onDragOver={handleDragOver}
-    onDrop={handleDrop}>
-        
+    <div className='main'>
+
+
+        <h1 style={{fontSize:"92px",fontWeight:999}}>{timer}</h1>
+
+        <button className='button-9' onClick={handleTimer}>start timer</button>
     
-  <h1>Drag and Drop Files here</h1>
-  { !files?<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_rwFHWUOYHzwhzoOYei7bEOOc2A4ny50zmQ&usqp=CAU'alt='' />:<img src={URL.createObjectURL(files)} alt=''/>}
   
-  <input type="file" multiple ref={input} style={{visibility:'hidden'}} onChange={handlefile}/>
-  <button onClick={clickSelectFile}>Select Files</button>
-  <button className='button-9' onClick={handleSubmit}>submit</button>
 
   </div>
 
