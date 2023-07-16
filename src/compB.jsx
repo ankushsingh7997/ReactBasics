39cf498ebff5ead5119fd3148a8b5e90c98b298b
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-07-16 16:46:13 +0530

    useContext

--- src/compB.jsx
+++ src/compB.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import CompC from './compC'
+import React, { useContext } from 'react'
+// import CompC from './compC'
+import { FirstName,LastName } from './App';
 
 const CompB = () => {
+    const fname=useContext(FirstName);
+    const lname=useContext(LastName);
   return (
     <div>
         
-        
-        <CompC/></div>
+        <h1> hii {fname} {lname}</h1>
+        <p>now we are using useContext() Hook</p>
+        {/* <CompC/> */}
+
+
+        </div>
   )
 }
 
